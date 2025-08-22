878cabe6bf8157734133286fd5f79b23a12d0c3c
Marcos Marques <marcos.marques@mosb.com.br>
2025-08-22 10:59:45 -0300

    update script deploy

--- deploy.sh
+++ deploy.sh
@@ -1,39 +1,29 @@
-#!/bin/bash
-
-# Variáveis
 BUILD_DIR="dist"
 BRANCH="build"
-MESSAGE="Deploy build"
 
-# Compila o projeto (ajuste se necessário)
-echo "Compilando o projeto..."
+git checkout -d $BRANCH
+
 tsc || { echo "Erro na compilação"; exit 1; }
 
-# Vai para a branch gh-pages (cria se não existir)
-echo "Criando branch temporária $BRANCH..."
-git checkout --orphan $BRANCH
+git rm -rf .
 
-# Remove todos os arquivos da árvore de trabalho (não mexe no .git)
-git rm -rf . > /dev/null 2>&1
-
-# Copia os arquivos do build para a raiz
-echo "Copiando arquivos do build..."
-cp -r ../$BUILD_DIR/* .
+#echo "Copiando arquivos do build..."
+#cp -r ../$BUILD_DIR/* .
 
 # Adiciona todos os arquivos do build
-git add .
+#git add .
 
 # Faz commit
-git commit -m "$MESSAGE"
+#git commit -m "deploy build"
 
-git remote add origin git@github.com:euMarcosEzequiel/module-api-types.git
+#git remote add origin git@github.com:euMarcosEzequiel/module-api-types.git
 
-git push origin $BRANCH --force
+#git push origin $BRANCH --force
 
-git checkout main
+#git checkout main
 
-git branch -d $BRANCH
+#git branch -d $BRANCH
 
-git reset --hard
+#git reset --hard
 
-echo "Deploy feito com sucesso!"
+#echo "Deploy feito com sucesso!"
