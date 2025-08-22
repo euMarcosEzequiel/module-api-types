1300af92285c1bbdb869e4ffd13bc53e4fad5afd
Marcos Marques <marcos.marques@mosb.com.br>
2025-08-22 11:23:08 -0300

    update script deploy

--- deploy.sh
+++ deploy.sh
@@ -5,18 +5,18 @@ git checkout -b $BRANCH
 
 tsc || { echo "Erro na compilação"; exit 1; }
 
-rm -rf .
+rm -rf !($BUILD_DIR)
 
-cp $BUILD_DIR/* .
+#cp $BUILD_DIR/* .
 
-rm -rf $BUILD_DIR
+#rm -rf $BUILD_DIR
 
-git add .
+#git add .
 
-git commit -m "deploy build"
+#git commit -m "deploy build"
 
-git push origin $BRANCH --force
+#git push origin $BRANCH --force
 
-git checkout main
+#git checkout main
 
-git reset --hard
+#git reset --hard
